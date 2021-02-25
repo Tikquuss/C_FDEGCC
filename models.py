fe4b5e3f3e603041d1aa6d10ddb2ab43981a7d80
Tikeng Notsawo Pascal Junior <pascalnotsawo@gmail.com>
2021-02-25 14:55:14 +0100

    Update models.py

--- models.py
+++ models.py
@@ -1,3 +1,10 @@
+# Copyright (c) 2021-present, Pascal Tikeng.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
 """ Transformer Model Classes & Config Class """
 
 import math
